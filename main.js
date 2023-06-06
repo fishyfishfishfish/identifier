774862a56debc925e0178195563405503f800179
fishyfishfishfish <114358433+fishyfishfishfish@users.noreply.github.com>
2023-06-05 20:39:29 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,56 +1,56 @@
-var img="";
 objects=[];
 status="";
+
 function preload(){
-    img=loadImage('dog_cat.jpg');
+    video=createVideo("video.mp4");
 }
 
 function setup(){
-    canvas=createCanvas(640,420);
+    canvas=createCanvas(480,380);
     canvas.center();
-    video=createCapture(VIDEO);
-    video.size(640,420);
     video.hide();
 }
 
-function start(){
-    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML="status: detecting objects"; 
-}
-
 function draw(){
-    
-    image(video,0,0,640,420);
+    image(video,0,0,480,380);
+
     if(status != ""){
-        R=random(255);
-        G=random(255);
-        B=random(255);
-        objectDetector.detect(video, gotResult);
+        objectDetector.detect(video,gotResult);
+
         for(i=0;i<objects.length;i++){
-            document.getElementById("status").innerHTML="status:objects detected";
-            document.getElementById("numberOfObjects").innerHTML="amount of objects detected:"+objects.length;
-            fill(R,G,B);
+            document.getElementById("status").innerHTML="status: objects detected";
+            document.getElementById("numberOfObjects").innerHTML="number of objects detected are:"+objects.length;
+
+            fill("green");
             percent=floor(objects[i].confidence * 100);
             text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
             noFill();
-            stroke(R,G,B);
+            stroke("red");
             rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
         }
     }
 }
 
-function modelLoaded(){
-    console.log("modelloaded");
-    status=true;
-}
-
 function gotResult(error,results){
     if(error){
-        console.log(error);
+        console.log(error)
     }
     else{
         console.log(results);
         objects=results;
     }
-
 }
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status: detecting objects";
+}
+
+function modelLoaded(){
+    console.log("model is loaded");
+    status=true;
+    video.speed(1);
+    video.loop();
+    video.volume(0);
+}
+
